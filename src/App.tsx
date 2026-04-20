8a471f755dd0e1a1ebc81f67de82b9a2e5b35905
Gnoud34 <nguyenduonggit299@gmail.com>
2026-04-21 00:03:41 +0700

    update sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard';
-import Tenants from './pages/Dashboard/Tenants.tsx'
+import Tenants from './pages/Tenants/Tenants'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
@@ -10,11 +10,11 @@ function App() {
       <Routes>
         {/* Automatically go to dashboard when opening the site */}
         <Route path="/" element={<Navigate to="/dashboard" />} />
-        
+
         {/* Main Routes */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/tenants" element={<Tenants />} />
-        
+
         {/* Placeholders for other pages */}
         <Route path="/rooms" element={<div>Rooms Page Coming Soon</div>} />
         <Route path="/maintenance" element={<div>Maintenance Page Coming Soon</div>} />
