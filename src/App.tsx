3952898f865fb22808ae5bec5e0928ed159b8f3d
Gnoud34 <nguyenduonggit299@gmail.com>
2026-04-21 00:46:51 +0700

    crud Rooms

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Tenants from './pages/Tenants/Tenants'
+import Rooms from './pages/Rooms/Rooms';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
         <Route path="/tenants" element={<Tenants />} />
 
         {/* Placeholders for other pages */}
-        <Route path="/rooms" element={<div>Rooms Page Coming Soon</div>} />
+        <Route path="/rooms" element={<Rooms />} />
         <Route path="/maintenance" element={<div>Maintenance Page Coming Soon</div>} />
       </Routes>
     </Router>
