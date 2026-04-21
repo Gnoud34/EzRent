08dae2f241c4153dec059b379ae226f172b41576
reversefall <nphutien.1704@gmail.com>
2026-04-21 21:18:17 +0700

    add tenant or guest request

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -9,18 +9,14 @@ const Sidebar: React.FC = () => {
         { name: 'Room', icon: 'bi-door-open-fill', path: '/rooms' },
         { name: 'Tenant', icon: 'bi-people-fill', path: '/tenants' },
         { name: 'Maintenance', icon: 'bi-tools', path: '/maintenance' },
+        { name: 'User Request', icon: 'bi-gear-fill', path: '/UserRequest' },
         { name: 'Setting', icon: 'bi-gear-fill', path: '/settings' },
     ];
 
     const handleLogout = () => {
         if (window.confirm('Bạn có chắc chắn muốn đăng xuất?')) {
-            // 1. Xóa dữ liệu user khỏi localStorage
             localStorage.removeItem('user');
-
-            // 2. Log ra console để kiểm tra (Cách 1 bạn đang dùng)
             console.log('Đã đăng xuất. LocalStorage hiện tại:', localStorage.getItem('user'));
-
-            // 3. Điều hướng về trang Login
             navigate('/login');
         }
     };
